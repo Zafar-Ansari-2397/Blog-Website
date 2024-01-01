d37799554e797f5cf125ddb8312baeef736cf531
Zafar Ansari <zafar.ansari.892@gmail.com>
2024-01-02 00:00:47 +0530

    initial commit

---
+++ src/components/TopBar.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import "./topbar.css";
+
+const TopBar = () => {
+  return (
+    <div className="top">
+      <div className="topLeft">
+        <i className="topIcon fa-brands fa-facebook"></i>
+        <i className="topIcon fa-brands fa-twitter"></i>
+        <i className="topIcon fa-brands fa-pinterest"></i>
+        <i className="topIcon fa-brands fa-square-instagram"></i>
+      </div>
+      <div className="topCenter">
+        <ui className="topList">
+          <li className="topListItem">Home</li>
+          <li className="topListItem">About</li>
+          <li className="topListItem">Contact</li>
+          <li className="topListItem">Write</li>
+          <li className="topListItem">Logout</li>
+        </ui>
+      </div>
+      <div className="topRight">
+        <img
+          src="https://c4.wallpaperflare.com/wallpaper/622/941/214/jujutsu-kaisen-satoru-gojo-hd-wallpaper-preview.jpg"
+          alt="profile pic"
+          className="topImg"
+        />
+        <i className="topSearchIcon fa-solid fa-magnifying-glass"></i>
+      </div>
+    </div>
+  );
+};
+
+export default TopBar;
