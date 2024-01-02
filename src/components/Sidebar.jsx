8f27efa843e1b4fc1593a3c75d433aa1d6cac127
Zafar Ansari <zafar.ansari.892@gmail.com>
2024-01-02 20:06:57 +0530

    add sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,7 +2,41 @@ import React from "react";
 import "../styles/sidebar.css";
 
 const Sidebar = () => {
-  return <div className="sidebar">Sidebar</div>;
+  return (
+    <div className="sidebar">
+      <div className="sidebarItem">
+        <span className="sidebarTitle">ABOUT ME</span>
+        <img
+          src="https://images.pexels.com/photos/534164/pexels-photo-534164.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          alt="profile"
+        />
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis,
+          suscipit repellendus temporibus odit quis quo optio doloribus
+        </p>
+      </div>
+      <div className="sidebarItem">
+        <span className="sidebarTitle">CATEGORIES</span>
+        <ul className="sidebarList">
+          <li className="sidebarListItem">Life</li>
+          <li className="sidebarListItem">Music</li>
+          <li className="sidebarListItem">Style</li>
+          <li className="sidebarListItem">Sport</li>
+          <li className="sidebarListItem">Tech</li>
+          <li className="sidebarListItem">Cinema</li>
+        </ul>
+      </div>
+      <div className="sidebarItem">
+        <span className="sidebarTitle">FOLLOW US</span>
+        <div className="sidebarSocial">
+          <i className="sidebarIcon fa-brands fa-facebook"></i>
+          <i className="sidebarIcon fa-brands fa-twitter"></i>
+          <i className="sidebarIcon fa-brands fa-pinterest"></i>
+          <i className="sidebarIcon fa-brands fa-square-instagram"></i>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;
